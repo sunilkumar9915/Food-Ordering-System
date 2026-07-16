eb041e4e01ad9dd7e90e0521794e59c0609c877b
124Ashish <guihgkiu@gmail.com>
2026-07-16 13:03:27 +0530

    Fix myOrders API URL

--- backend/frontend/myOrders.js
+++ backend/frontend/myOrders.js
@@ -14,7 +14,7 @@ async function loadMyOrders() {
 
     try {
 
-        const res = await fetch(`https://food-ordering-system.up.railway.app/api/orders/${user._id}`);
+        const res = await fetch(`https://food-ordering-system-production-abb0.up.railway.app/api/orders/${user._id}`);
 
         const data = await res.json();
 
